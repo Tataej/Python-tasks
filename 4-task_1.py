d59bfa78cbbba632dcc7444a3655c7d0251d7f79
Tetiana <tania.ejik@gmail.com>
2020-04-13 14:54:15 +0300

    HW_4

---
+++ 4-task_1.py
@@ -0,0 +1,3 @@
+text = 'Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. Complex is better than complicated. Readability counts.'
+print(text.lower())
+print(text.upper())
---
+++ 4-task_2.py
@@ -0,0 +1,4 @@
+print("Input number from 4 digits: ")
+number = str(input(''))
+number1 = number.split()
+print(number1[0])
